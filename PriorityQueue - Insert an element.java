f3676fd3f50c4f3d2171a6ad4bfab3869a80daa1
Ihsan <ihsanmpm@gmail.com>
2020-02-24 20:57:40 +0530

    Create PriorityQueue - Insert an element.java

---
+++ PriorityQueue - Insert an element.java
@@ -0,0 +1,17 @@
+import java.util.PriorityQueue;
+public class Main {
+  public static void main(String[] args) {
+  // Write your code here
+  PriorityQueue<String> queueColors = new PriorityQueue<>();
+  
+  queueColors.add("blue");
+  queueColors.add("red");
+  queueColors.add("green");
+  queueColors.add("yellow");
+  queueColors.add("pink");
+  queueColors.add("magento");
+  queueColors.add("orange");
+  queueColors.add("cyan");
+  
+  System.out.println("Colors in the Priority Queues are: ");  
+  System.out.println(queueColors);
